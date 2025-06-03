c7c516b1914aa08a8f3e3ce459eeef7a58db1879
Kaspar Kallas <git@kasparkallas.com>
2025-06-03 16:17:16 +0300

    remove quotes

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function Layout({ children }: { children: ReactNode }) {
         <RootProvider
           search={{
             options: {
-              type: 'static',
+              // type: 'static',
             },
           }}
         >
