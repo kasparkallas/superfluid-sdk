0c8d06c130ee3dc64d6267c40966cf5d8a76ea77
Kaspar Kallas <git@kasparkallas.com>
2025-06-03 11:36:46 +0300

    add fonts

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,15 +1,17 @@
 import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
+import { Open_Sans } from 'next/font/google';
 import type { ReactNode } from 'react';
 
-const inter = Inter({
+const openSans = Open_Sans({
   subsets: ['latin'],
+  variable: '--font-open-sans',
+  weight: ['300', '400', '500', '600', '700'],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" className={openSans.variable} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
